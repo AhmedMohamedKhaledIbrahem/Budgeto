6fc549971ed6271729f98964d7dba517e614a0d6
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2026-07-22 15:33:48 +0300

    add SpendingDropdownMenu component and FilterSelection sealed interface to filter transactions in the spending money feature

---
+++ app/src/main/java/com/budgeto/feature/spendingmoney/presentation/screen/component/SpendingDropdownMenu.kt
@@ -0,0 +1,98 @@
+package com.budgeto.feature.spendingmoney.presentation.screen.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.budgeto.core.ui.spacing
+import com.budgeto.feature.spendingmoney.domain.enums.CategoryType
+import com.budgeto.feature.spendingmoney.domain.enums.SpendingType
+
+@Composable
+fun SpendingDropdownMenu(
+    modifier: Modifier = Modifier,
+    onFilterSelected: (FilterSelection) -> Unit,
+    isMenuExpanded: Boolean,
+    onMenuExpandedChanged: (Boolean) -> Unit
+) {
+    DropdownMenu(
+        expanded = isMenuExpanded,
+        onDismissRequest = { onMenuExpandedChanged(false) }
+    ) {
+        DropdownMenuItem(
+            text = {
+                Text(
+                    text = "All Transactions",
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.onSurface,
+                )
+            },
+            onClick = {
+                onFilterSelected(FilterSelection.All)
+                onMenuExpandedChanged(false)
+            }
+        )
+        HorizontalDivider(modifier = modifier.padding(vertical = MaterialTheme.spacing.spaceSix))
+        Text(
+            text = "Categories",
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(
+                horizontal = MaterialTheme.spacing.spaceTwelve,
+                vertical = MaterialTheme.spacing.spaceSix
+            )
+        )
+        CategoryType.entries.forEach { categoryType ->
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        text = "Category Type",
+                        style = MaterialTheme.typography.labelSmall,
+                        color = MaterialTheme.colorScheme.onSurface,
+                    )
+                },
+                onClick = {
+                    onFilterSelected(FilterSelection.Category(categoryType))
+                    onMenuExpandedChanged(false)
+                }
+            )
+        }
+        HorizontalDivider(modifier = modifier.padding(vertical = MaterialTheme.spacing.spaceSix))
+        Text(
+            text = "Spending",
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = modifier.padding(
+                horizontal = MaterialTheme.spacing.spaceTwelve,
+                vertical = MaterialTheme.spacing.spaceSix
+            )
+        )
+        SpendingType.entries.forEach { spending ->
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        text = "Payment Methods",
+                        style = MaterialTheme.typography.labelSmall,
+                        color = MaterialTheme.colorScheme.onSurface,
+                    )
+                },
+                onClick = {
+                    onFilterSelected(FilterSelection.PaymentType(spending))
+                    onMenuExpandedChanged(false)
+                }
+            )
+        }
+
+    }
+}
+
+
+sealed interface FilterSelection {
+    data object All : FilterSelection
+    data class Category(val category: CategoryType) : FilterSelection
+    data class PaymentType(val spendingType: SpendingType) : FilterSelection
+}
