4ff62b6c6657065c15df89659b79bacec847c3bc
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2026-07-26 15:43:52 +0300

    update BudgetoDataBase version to 2

--- app/src/main/java/com/budgeto/core/database/BudgetoDataBase.kt
+++ app/src/main/java/com/budgeto/core/database/BudgetoDataBase.kt
@@ -12,7 +12,7 @@ import com.budgeto.core.database.entity.SpendingEntity
         MonthlyBudgetEntity::class,
         SpendingEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false,
 )
 abstract class BudgetoDataBase: RoomDatabase() {
